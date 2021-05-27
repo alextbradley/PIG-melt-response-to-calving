a7eeb960172ab14f600f6081029f613c1a10d861
Alexander Bradley <aleey@bslws01.nerc-bas.ac.uk>
2021-05-27 12:21:32 +0100

    added figure 4 script and image

---
+++ make_figures/make_figure3.m
@@ -0,0 +1,1 @@
+%Make figure 3 in the Idealized PIG calving manuscript
--- make_figures/plot_defaults.m
+++
@@ -1,5 +0,0 @@
-%Set plot defaults
-
-set(0, 'defaultlinelinewidth', 1.5);
-set(0, 'defaultaxeslinewidth', 1.5);
-set(0, 'defaultaxesfontsize', 10);

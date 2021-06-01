8b9778357613ec181d2440851c809d515e5eb6bb
Alexander Bradley <aleey@bslws01.nerc-bas.ac.uk>
2021-06-01 14:48:49 +0100

    added bare figure 5 script

---
+++ make_figures/make_figure5.m
@@ -0,0 +1,11 @@
+%Make figure 5 in the Idealized PIG calving manuscript: results from the default run.
+%Column 1: Melt rate and BSF contours 
+%Column 2: Zonal Sections velocity
+%Column 3: Zonal Sections temperature
+%Column 4: Meridional Sections
+%
+% NB: Many of the data files referred to in this script are too large to be hosted online. These files are hosted internally as BAS.
+% Please email Alex Bradley (aleey@bas.ac.uk) to obtain a copy.
+%Alex Bradley (aleey@bas.ac.uk) 27/05/2021. MIT license.
+
+

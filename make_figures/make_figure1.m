44890248f456f3a86d4ed77670d8095941ba5cfe
Alex Bradley <alextbradley.1@gmail.com>
2021-08-04 14:09:56 +0100

    added water column thickness to figure 1

--- make_figures/make_figure1.m
+++ make_figures/make_figure1.m
@@ -8,9 +8,10 @@
 saveflag = 0;
 addpath('plot_tools');
 plot_defaults
-positions = [0.1,0.1,0.4,0.85;
-             0.58, 0.31, 0.38, 0.25;
-             0.58, 0.59, 0.38, 0.25];
+positions = [0.06,0.37,0.42,0.53; %bathymetry depth
+	     0.56,0.37,0.42,0.53; %ridge-draft gap	
+             0.06, 0.1, 0.42, 0.25;
+             0.56, 0.1, 0.42, 0.25];
 
 close all
 
@@ -73,7 +74,10 @@ end
 % plot bathy and topos and get data
 %
 figure(1); clf; hold on
-contourf(x,y,bathyng', 20, 'linestyle', 'none')
+gap = topo2009 - bathy;
+%contourf(x,y,bathyng', 20, 'linestyle', 'none')
+contourf(x,y,gap', 50, 'linestyle', 'none')
+
 [cgl,~] = contour(x,y,bathy', [0,0], 'k');
 [c2009,~] = contour(x,y,topo2009', [0,0], 'r');
 [c2020,~] = contour(x,y,topo2020', [0,0], 'b');
@@ -82,12 +86,31 @@ contourf(x,y,bathyng', 20, 'linestyle', 'none')
 %
 %transect
 %
-yidx = 5:140;
-xidx = 256*ones(1,length(yidx));
+%yidx = 5:140;
+%xidx = 256*ones(1,length(yidx));
+
+np = 50; %nmber of pts on each half of transect
+cx = 245;
+cy = 73;
+xidx1 = round(linspace(257, cx, np));
+yidx1 = round(linspace(5, cy, length(xidx1)));
+%p1 =  plot(x(xidx1), y(yidx1), 'ro');
+
+xidx2 = round(linspace(cx, 259, np));
+yidx2 = round(linspace(cy, 140, length(xidx2)));
+%p2 =  plot(x(xidx2), y(yidx2), 'bo');
+
+xidx = [xidx1, xidx2];
+yidx = [yidx1, yidx2];
+
+
 sline =  sqrt((x(xidx) - x(xidx(1))).^2 + (y(yidx) - y(yidx(1))).^2); %arclength along line
 xline = x(xidx);
 yline = y(yidx);
 plot(xline, yline, 'k--', 'linewidth', 2);
+
+
+%error()
 
 %
 %get bathy and topo along line
@@ -107,24 +130,25 @@ sline = sline(idx);
 sline = sline - sline(1);
 bathyline = bathyline(idx);
 topoline = topoline(idx);
-%
-% Open image
-%
- 
-t = Tiff('PIG-S2-NovDec2020.tif', 'r'); %!! not in git repo!!
-imageData = read(t);
-figure(2); 
-ax(1) = subplot('Position',positions(1,:)); imshow(imageData);
 
 %
-% add bathymetry and fronta
+% Open image
+% 
+t = Tiff('PIG-S2-NovDec2020.tif', 'r'); %!! not in git repo!!
+imageData = read(t);
+figure(2);
+
 %
+% Plot 1: bathymetry
+%
+ax(1) = subplot('Position',positions(1,:)); imshow(imageData);
+
+% add bathymetry and fronta
 hold on
 contourf(xi, yi, bathyng', 30, 'linestyle', 'none'); 
 c = colorbar('Location', 'northoutside');
-c.Position(2) = 0.79;
+c.Position(2) = 0.91;
 c.Position(4) = 0.02;
-
 c.Label.String = 'sea bed depth (m)';
 c.Label.VerticalAlignment = 'bottom';
 c.Label.FontSize = 12;
@@ -133,20 +157,15 @@ c.Label.Interpreter = 'latex';
 [cgl,~] = contour(xi,yi,bathy', [0,0], 'k', 'linewidth',2 );
 [c2009,h2009] = contour(xi,yi,topo2009', [0,0], 'linecolor', plotcolor3, 'linewidth', 2);
 [c2020,h2020] = contour(xi,yi,topo2020', [0,0], 'linecolor', plotcolor1, 'linewidth', 2);
-95
 
-%
 % add cross section
-%
 cline = [xline; yline];
 cline_img = model2image(cline);
 plot(cline_img(1,:), cline_img(2,:), 'k--');
 ptA = text(ax(1), 9700,8200, 'A', 'FontSize',12, 'FontWeight', 'bold');
 ptB = text(ax(1), 5300,8900, 'B', 'FontSize',12, 'FontWeight', 'bold');
 
-%
 % Label fronts
-%
 f2009 = text(ax(1), 4500,13500, '2009', 'FontSize',12, 'color', plotcolor3);
 f2020 = text(ax(1), 8700,10200, '2020', 'FontSize',12, 'color', plotcolor1);
 
@@ -155,57 +174,99 @@ ylim([3000, 14847])
 xlim([2000,12000])
 plot(ax(1), [11000,11000], [5000,4000], 'k', 'linewidth', 3)
 scalebar = text(ax(1),10950, 6600, '10 km', 'Interpreter', 'latex', 'FontSize', 12);
-
-
 camroll(-90);
+%txa = text(ax(1), 250, 16000, '(a)', 'FontSize', 12, 'interpreter', 'latex');
 
-txa = text(ax(1), 250, 16000, '(a)', 'FontSize', 12, 'interpreter', 'latex')
+%
+% Plot 2: Ridge-draft gap
+%
+ax(2) = subplot('Position',positions(2,:)); imshow(imageData);
 
+% add bathymetry and fronta
+hold on
+contourf(xi, yi, gap', 30, 'linestyle', 'none'); 
+d = colorbar('Location', 'northoutside');
+d.Position(2) = c.Position(2);
+d.Position(4) = c.Position(4);
+d.Label.String = 'water column thickness (m)';
+d.Label.VerticalAlignment = 'bottom';
+d.Label.FontSize = 12;
+d.Label.Interpreter = 'latex';
+
+[cgl,~] = contour(xi,yi,bathy', [0,0], 'k', 'linewidth',2 );
+[c2009,h2009] = contour(xi,yi,topo2009', [0,0], 'linecolor', plotcolor3, 'linewidth', 2);
+[c2020,h2020] = contour(xi,yi,topo2020', [0,0], 'linecolor', plotcolor1, 'linewidth', 2);
+
+% add cross section
+cline = [xline; yline];
+cline_img = model2image(cline);
+plot(cline_img(1,:), cline_img(2,:), 'k--');
+ptA = text(ax(2), 9700,8200, 'A', 'FontSize',12, 'FontWeight', 'bold');
+ptB = text(ax(2), 5300,8900, 'B', 'FontSize',12, 'FontWeight', 'bold');
+
+% Label fronts
+%f2009 = text(ax(2), 4500,13500, '2009', 'FontSize',12, 'color', plotcolor3);
+%f2020 = text(ax(2), 8700,10200, '2020', 'FontSize',12, 'color', plotcolor1);
+
+%tidy
+ylim([3000, 14847])
+xlim([2000,12000])
+%plot(ax(2), [11000,11000], [5000,4000], 'k', 'linewidth', 3)
+%scalebar = text(ax(1),10950, 6600, '10 km', 'Interpreter', 'latex', 'FontSize', 12);
+camroll(-90);
 
 %
 % Plot along the cross section
 %
-ax(2) = subplot('Position', positions(3,:)); grid on 
+ax(3) = subplot('Position', positions(3,:)); grid on 
 plot(sline/1e3, bathyline, 'color', plotcolor1, 'linewidth', 2);
 hold on
 plot(sline/1e3, topoline, 'color', plotcolor3, 'linewidth', 2);
 xlim([0, 45])
-ax(2).XTickLabels = cell(length(ax(2).XTick), 1);
+%ax(3).XTickLabels = cell(length(ax(3).XTick), 1);
 
-ax(3) = subplot('Position', positions(2,:)); grid on
+ax(4) = subplot('Position', positions(4,:)); grid on
 plot(sline/1e3,topoline - bathyline, 'color', plotcolor1, 'linewidth', 2);
 xlim([0, 45])
-ptAA = text(ax(2), 1,-700, 'A', 'FontSize', 12, 'FontWeight', 'bold');
-ptBB = text(ax(2), max(ax(3).XLim)-3 ,-700, 'B', 'FontSize', 12, 'FontWeight', 'bold');
+ptAA = text(ax(4), 1,-700, 'A', 'FontSize', 12, 'FontWeight', 'bold');
+ptBB = text(ax(4), max(ax(4).XLim)-3 ,-700, 'B', 'FontSize', 12, 'FontWeight', 'bold');
 
 
 
 %
 % tidy everything
 %
+ax(4).XLabel.String = 'Distance along transect (km)';
 ax(3).XLabel.String = 'Distance along transect (km)';
+ax(4).XLabel.Interpreter = 'latex';
 ax(3).XLabel.Interpreter = 'latex';
+ax(4).XLabel.FontSize = 12;
 ax(3).XLabel.FontSize = 12;
-ax(2).YLabel.String = 'depth (m)';
-ax(2).YLabel.FontSize = 12;
-ax(2).YLabel.Interpreter = 'latex';
-ax(2).YLim = [-850, -200];
-ax(2).YTick = [-800, -600, -400,-200];
-ax(3).YTick = [0,100,200,300, 400];
-ax(3).YLabel.String = 'gap (m)';
+ax(3).YLabel.String = 'depth (m)';
+ax(3).YLabel.FontSize = 12;
+ax(3).YLabel.Interpreter = 'latex';
+ax(3).YLim = [-850, -200];
+ax(3).YTick = [-800, -600, -400,-200];
+ax(4).YTick = [0,100,200,300, 400];
+ax(4).YLabel.String = 'gap (m)';
+ax(4).YLabel.FontSize = 12;
+ax(4).YLabel.Interpreter = 'latex';
 ax(3).YLabel.FontSize = 12;
 ax(3).YLabel.Interpreter = 'latex';
 
-txb = text(ax(2), -8, 80, '(b)', 'FontSize', 12, 'interpreter', 'latex')
-txc = text(ax(3), -8, 420, '(c)', 'FontSize', 12, 'interpreter', 'latex')
+%txb = text(ax(3), -8, 80, '(b)', 'FontSize', 12, 'interpreter', 'latex')
+%txc = text(ax(4), -8, 420, '(c)', 'FontSize', 12, 'interpreter', 'latex')
 
 fig = gcf; fig.Position(3:4) = [900, 600];
 grid(ax(3), 'on')
-grid(ax(2), 'on')
+grid(ax(4), 'on')
 
 %
+ax(3).YLabel.FontSize = 12;
+ax(3).YLabel.Interpreter = 'latex';
 % Save?
 %
 if saveflag
-saveas(gcf, 'plots/figure1.eps', 'epsc');
+%saveas(gcf, 'plots/figure1.eps', 'epsc');
+saveas(gcf, "plots/figure1_withColumnThickness.eps", "epsc")
 end

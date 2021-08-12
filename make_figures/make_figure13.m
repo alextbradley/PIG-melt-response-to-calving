9adcbc6859eea0e0180664607d81f315a4cd7c17
Alex Bradley <alextbradley.1@gmail.com>
2021-08-12 18:51:24 +0100

    changed file names on figs 11,12,13

--- make_figures/make_figure13.m
+++ make_figures/make_figure13.m
@@ -1,4 +1,5 @@
-%Make figure 13 of the PIG calving manuscript: plots of barotropic stream function (contours) and water column thickness (colours) for each of the scenarios.
+%Make figure 13 of the PIG calving manuscript: mean melt rate, millgate decomposition for the two regions. Neeed to run make_figure10 first to get the 'snap distance'
+
 
 % NB: Many of the data files referred to in this script are too large to be hosted online. These files are hosted internally as BAS.
 % Please email Alex Bradley (aleey@bas.ac.uk) to obtain a copy.
@@ -7,25 +8,21 @@
 %
 % Flags
 %
-gendata = 0; %specify whether to pass through the generate data loop
-save_flag = 0;
+gendata = 1; %specify whether to pass through the generate data loop
+saveflag = 0;
 
 %
 % Preliminaries
 %
 addpath("plot_tools");
 plot_defaults
+plotcolor2 = [0,1,1]; %overwrite defaults
+plotcolor3 = [1,0,1];
 label_size = 11;
 ax_fontsize = 11;
-figure(2); clf;
+figure(3); clf;
 fig = gcf; fig.Position(3:4) = [1085, 540];
-%plot colours
-background_color = 0.8*[1,1,1]; %ice color
-background_color = [1,1,1];
-ocean_color = [226,249, 255]/255;
-cols = [7,43, 184;
-        184, 7, 43]/255; %colours for each different bc
-bathycontourcolor = [255,0,255]/255;
+
 
 %
 % Data locations
@@ -45,28 +42,20 @@ X = ncread(strcat(rootdir, "078", '/run/state2D.nc'), 'LONGITUDE');
 Y = ncread(strcat(rootdir, "078", '/run/state2D.nc'), 'LATITUDE');
 [XX,YY] = meshgrid(X,Y);
 YYt = YY';
-
-%put onto the latlon grid
-xshift = 870;
-yshift = -15000;
-lambdashift = -101;
-phishift = -0.07;
-XX = XX + xshift;
-YY = YY + yshift;
-[phi, lambda] = polarstereo_inv(XX,YY);
-lambda = lambda + lambdashift;
-phi = phi + phishift;
-lambda = double(lambda);
-phi = double(phi);
+Z = 0:dz:(nz-1)*dz;
 
 
 %parameters
 secs_per_year = 365.25*24*60*60;
 density_ice = 918.0;
+lambda = 7.61*1e-4;%constants in liquidus
+gamma = 5.73*1e-2;
+T0 = 8.32*1e-2;
 
 %time details
-ntout1 = 11;
-ntout2 = 12; %define time period to average over
+ntout1 = 6;
+ntout2 = 7; %define time period to average over
+
 
 
 %
@@ -74,12 +63,15 @@ ntout2 = 12; %define time period to average over
 %
 if gendata
 run_nos = ["078", "082", "083", "084", "085", "086"];
-%run_nos = ["141", "142", "143", "144", "145", "146"];
+run_nos = ["141", "142", "143", "144", "145", "146"];
 sz = length(run_nos);
 
 %setup storage
-bsf_scenarios = cell(1,sz);
+melt_scenarios = cell(1,sz);
 topo_scenarios = cell(1,sz);
+Tbl_scenarios = cell(1,sz);
+Sbl_scenarios = cell(1,sz);
+ustar_scenarios = cell(1,sz);
 
 %load bathy
 bathyfid = fopen(bathypath);
@@ -96,140 +88,183 @@ topo = fread(topo_fid, 'real*8', 'b');
 topo = reshape(topo, [nx,ny]);
 topo_scenarios{i} = topo;
 
-%velocities
-UVEL_fname = strcat(rootdir, run_nos(i), '/run/stateUvel.nc');
-UVEL = ncread(UVEL_fname, 'UVEL', [1,1,1,ntout1], [Inf, Inf, Inf, 1+ntout2 - ntout1]);
-VVEL_fname = strcat(rootdir, run_nos(i), '/run/stateVvel.nc');
-VVEL = ncread(VVEL_fname, 'VVEL', [1,1,1,ntout1], [Inf, Inf, Inf, 1 + ntout2 - ntout1]);
-VVEL = squeeze(mean(VVEL,4));
-UVEL = squeeze(mean(UVEL,4));
-nt = length(ncread(UVEL_fname, 'TIME'))
+%melt rates
+state2D_fname = strcat(rootdir, run_nos(i), '/run/state2D.nc');
+melt = ncread(state2D_fname, 'SHIfwFlx', [1, 1, ntout1], [Inf, Inf, 1+ntout2- ntout1]);
+melt = mean(melt, 3); %average over months ntout1 to ntout2
+melt = -melt * secs_per_year / density_ice;
+melt_scenarios{i} = melt;
 
+%u star
+ubl = ncread(state2D_fname, 'SHIuLoc', [1, 1, ntout1], [Inf, Inf, 1+ntout2- ntout1]);
+vbl = ncread(state2D_fname, 'SHIvLoc', [1, 1, ntout1], [Inf, Inf, 1+ntout2- ntout1]);
+ubl = mean(ubl,3);
+vbl = mean(vbl,3);
+ustar = sqrt(ubl.^2 + vbl.^2);
+ustar_scenarios{i} = ustar;
 
-%compute bsf
-vvel = squeeze(sum(VVEL, 3)) * dz; %units m^2 /s
-stream=zeros(size(vvel));
-stream(nx,:)=vvel(nx,:)*dx;
-for p=nx-1:-1:1
- stream(p,:)=stream(p+1,:) + vvel(p,:)*dx;
-end
-stream = stream/1e6; %convert to sv
-bsf_scenarios{i} = stream;
+%Theta
+Theta_fname = strcat(rootdir, run_nos(i), '/run/stateTheta.nc');
+Theta = ncread(Theta_fname, 'THETA', [1,1,1,ntout1], [Inf, Inf, Inf, 1+ntout2 - ntout1]);
+Theta = mean(Theta, 4);
+Salt_fname = strcat(rootdir, run_nos(i), '/run/stateSalt.nc');
+Salt = ncread(Salt_fname, 'SALT', [1,1,1,ntout1], [Inf, Inf, Inf, 1+ntout2 - ntout1]);
+Salt = mean(Salt, 4);
 
-end
-end
+Sbl = zeros(nx,ny);
+Tbl = zeros(nx,ny);
+Nb = 3; %number of grid cells to average over
+for p = 1:nx
+for q = 1:ny
+        if topo(p, q) < 0 %if we're in the cavity
+                draft = topo(p,q);
+                partial_cell_frac = abs(rem(draft, dz)) / dz;
+                draft_rounded = draft + abs(rem(draft, dz));
+                [~,idxtop] = min(abs(-Z - draft_rounded));
+                vec = [partial_cell_frac,1,1]';
+                Sbl(p,q) = sum(vec.*squeeze(Salt(p,q,idxtop:idxtop+Nb-1)))/sum(vec);
+                Tbl(p,q) = sum(vec.*squeeze(Theta(p,q,idxtop:idxtop+Nb-1)))/sum(vec);
+	end
+end %end loop over y
+end %end loop over x
+Tbl_scenarios{i} = Tbl;
+Sbl_scenarios{i} = Sbl;
 
-%
-% Plot preliminaries
-%
-width = 0.30;
-height = 0.44;
-gapx = 0.02;
-gapy = 0.02;
-ncols = 3;
-nrows = 2;
-startx = (1 -width*ncols - (ncols-1)*gapx)/2;
-starty = 1-(1 -height*nrows - (nrows -1)*gapy)/2;
-starty = 0.97;
-positions = zeros(4, nrows* ncols);
-for i = 1:nrows*ncols
-q = 1 + mod(i-1,ncols); %index in x direction
-p = ceil(i/ncols); %index in y directio
-positions(:,i) = [startx + (q-1)*gapx + (q-1)*width, starty - p*height - (p-1)*gapy, width, height];
-end
-cmap = lighter_blue_parula(100,0.1);
-
-%
-% Make plots sequentially
-%
-for q = 1:sz
-%get data
-icetopo = cell2mat(topo_scenarios(q));
-bsf = cell2mat(bsf_scenarios(q));
-topo = cell2mat(topo_scenarios(q));
-h = topo - bathy;
-h(bathy == 0) = nan;
-invh = 1./h;
-invh = saturate(invh, 0.01, 0);
-bsf(bathy == 0) = nan;
-bsf_sat = saturate(bsf, 0.45, -0.4);
-
-%bottom layer: inverse water column thickness
-axs(q) = subplot('Position',positions(:,q));
-contourf(lambda,phi,invh', 15, 'linestyle', 'none')
-colormap(axs(q),cmap);
-if q == 1
-c = colorbar;
-c.Location = 'west';
-%c.Position(1) = positions(1,q) + 0.005;  %left of plot
-c.Position(1) = 0.3;
-c.Position(2) = 0.8;
-c.Position(3) = 0.012; %make it thin
-c.Position(end) = 0.145;
-c.Color = 0*[1,1,1]; %set color to white/blck
-c.Label.String = '$1/h$ (m\textsuperscript{-1})';
-c.Label.FontSize = 10;
-c.Label.Interpreter = 'latex';
-end
-
-
-%top layer: contours of bsf, GL and ice front
-axnew(q) = axes; hold on %new axes for contours
-axnew(q).Position = axs(q).Position;
-[C,h] =contour(lambda, phi, bsf', -0.5:0.1:0.5, 'k', 'linewidth', 1);
-clabel(C,h)
-contour(lambda,phi,bathy', [0,0], 'k', 'linewidth', 1.5) %grounding line
-contour(lambda,phi,icetopo', [0,0], 'k', 'linewidth', 1.5) %ice front
-
-        
-%tidy plot
-grid(axs(q), 'on');
-axs(q).XLim= [-102.6, -98.8];
-axs(q).YLim =[-75.45,-74.75];
-axnew(q).XLim = axs(q).XLim;
-axnew(q).YLim = axs(q).YLim;
-axnew(q).Visible = 'off'; %make top layer invisible - see colours on bottom layer
-set(axs(q),'Color',background_color)
-axs(q).YTick = -75.4:0.1:-74.8;
-if mod(q,3) == 1 %label x axis on bottom row
-axs(q).YTickLabel = {"24'", "18'",  "12'", "6'",  "75S", "54'", "48'"};
-else %top row no labels
-axs(q).YTickLabel = cell(length(axs(q).YTick),1);
-end
-axs(q).XTick = [-102.5:0.5:-99];
-if floor((q-1)/3) == 1 %label y axis on left-most column only
-axs(q).XTickLabel = {"30'", "102W",  "30'", "101W", "30'", "100W", "30'", "99W"};
-else
-axs(q).XTickLabel = cell(length(axs(q).XTick),1);
-end
-
-%add the definitions of the two cavity regions
-realistic_inner_cavity_definition; %bring inner cavity definition into scope (a1,b1,a2,b2)
-[XXns,YYns] = meshgrid(X,Y);
-in1 = inpolygon(XXns',YYns', a1,b1);
-in2 = inpolygon(XXns',YYns', a2,b2);
-idx1 = (icetopo < 0) & in1;
-idx2 = (icetopo < 0) & in2;
-if ((q == 1) || (q == 6))
-bsf_copy = bsf;
-bsf_copy(idx1) = 0.01;
-bsf_copy(~idx1) =0;
-contour(lambda,phi,bsf_copy',[.01,.01], '--', 'linewidth', 1.5, 'linecolor', 'c')
-bsf_copy = bsf_sat;
-bsf_copy(idx2) = 0.01;
-bsf_copy(~idx2) =0;
-contour(lambda,phi,bsf_copy',[.01,.01], '--', 'linewidth', 1.5, 'linecolor', 'm')
-end
 end %end loop over runs
+end
 
 %
-% macro tidy
+% Plots
 %
-fig = gcf; fig.Position(3:4) = [1280, 687.333];
+positions = [0.1,0.1,0.4,0.85;
+             0.6, 0.55, 0.38, 0.4;
+             0.6, 0.1, 0.38, 0.4];
+
+
+realistic_inner_cavity_definition; %bring inner cavity definition into scope
+in1 = inpolygon(XX',YY', a1,b1);
+in2 = inpolygon(XX',YY', a2,b2);
+
+topo = cell2mat(topo_scenarios(1));
+idx1 = (topo < 0) & in1;
+idx2 = (topo < 0) & in2;
+for i = 1:sz
+melt = cell2mat(melt_scenarios(i));
+ave_melt1(i) = mean(melt(idx1));
+ave_melt2(i) = mean(melt(idx2));
+end
+ax(1) = subplot(2,2,1); hold on; box on; grid on
+plot(snap_distance(4)*ones(1,2)/1e3, [0, 100], 'k--', 'HandleVisibility', 'off')
+plot(snap_distance/1e3, ave_melt1, 'o-', 'color', plotcolor2, 'markerfacecolor', plotcolor2);
+
+ax(3) = subplot(2,2,3); hold on; box on; grid on
+plot(snap_distance(4)*ones(1,2)/1e3, [0, 100], 'k--', 'HandleVisibility', 'off')
+plot(snap_distance/1e3, ave_melt2, 'o-', 'color', plotcolor3, 'markerfacecolor', plotcolor3);
+
+%tidy these
+ax(1).YLim = [34, 38];
+ax(1).YTick = 34:38;
+ax(3).YLim = [65, 69];
+ax(3).YTick = 65:69;
+ax(1).XLabel.String = '$l_c$ (km)';
+ax(3).XLabel.String = '$l_c$ (km)';
+ax(1).YLabel.Interpreter = 'latex';
+ax(1).XLabel.Interpreter = 'latex';
+ax(3).YLabel.Interpreter = 'latex';
+ax(3).XLabel.Interpreter = 'latex';
+ax(1).YLabel.String = 'mean melt rate (m/yr)';
+ax(3).YLabel.String = 'mean melt rate (m/yr)';
+ax(1).XLabel.FontSize = 12;
+ax(1).YLabel.FontSize = 12;
+ax(3).XLabel.FontSize = 12;
+ax(3).YLabel.FontSize = 12;
+tN = text(ax(1), 1, 37.6, "North box", "Interpreter", "latex", "FontSize", 14, "Color", 'k');
+tS = text(ax(3), 1, 68.6, "South box", "Interpreter", "latex", "FontSize", 14, "Color", 'k');
+txt2009 = text(ax(1), 1.5, 34.2, '2009', 'Interpreter', 'latex', 'FontSize', 10, 'rotation', 90);
+txt2020 = text(ax(1), 18, 34.5, '2020', 'Interpreter', 'latex', 'FontSize', 10, 'rotation', 90);
+
+txt2009a = text(ax(3), 1.5, 65.5, '2009', 'Interpreter', 'latex', 'FontSize', 10, 'rotation', 90);
+txt2020a = text(ax(3), 18, 67, '2020', 'Interpreter', 'latex', 'FontSize', 10, 'rotation', 90);
+
 
 %
-% Save
+% decompositions
 %
-if save_flag
-saveas(gcf, "plots/figure13.eps", "epsc")
+
+%set up storage
+relmelt        = zeros(2,sz);
+relmelt_noTemp = zeros(2,sz);
+relmelt_noVel  = zeros(2,sz);
+%get the baseline velocity, theta, salt
+topo = cell2mat(topo_scenarios(1));
+ustar_baseline = cell2mat(ustar_scenarios(1));
+Tbl_baseline = cell2mat(Tbl_scenarios(1));
+Sbl_baseline = cell2mat(Sbl_scenarios(1));
+Tl_baseline  = T0 + lambda*topo - gamma*Sbl_baseline;
+UdT_baseline = ustar_baseline .* (Tbl_baseline - Tl_baseline);
+
+for j = 1:sz
+
+%get the current velocity, theta, salt
+ustar = cell2mat(ustar_scenarios(j));
+Tbl = cell2mat(Tbl_scenarios(j));
+Sbl = cell2mat(Sbl_scenarios(j));
+topo = cell2mat(topo_scenarios(j));
+Tl  = T0 + lambda*topo - gamma*Sbl; %liquidus temperature in BL
+
+%compute relative melt contributions
+UdT = ustar .* (Tbl - Tl);
+UdT_noVel =  ustar_baseline .* (Tbl - Tl);
+UdT_noTemp = ustar.* (Tbl_baseline - Tl_baseline);
+
+
+relmelt(1,j) = nanmean(UdT(idx1)) / nanmean(UdT_baseline(idx1));
+relmelt_noTemp(1,j) = nanmean(UdT_noTemp(idx1)) / nanmean(UdT_baseline(idx1));
+relmelt_noVel(1,j) = nanmean(UdT_noVel(idx1)) / nanmean(UdT_baseline(idx1));
+
+relmelt(2,j) = nanmean(UdT(idx2)) / nanmean(UdT_baseline(idx2));
+relmelt_noTemp(2,j) = nanmean(UdT_noTemp(idx2)) / nanmean(UdT_baseline(idx2));
+relmelt_noVel(2,j) = nanmean(UdT_noVel(idx2)) / nanmean(UdT_baseline(idx2));
+
+end
+
+plot_defaults %rerun to return to default colours
+for p = 1:2
+ax(2*p) = subplot(2,2,2*p); hold on;box on; grid on;
+plot(snap_distance(4)*ones(1,2)/1e3, [0.95, 1.15], 'k--', 'HandleVisibility', 'off')
+plot(snap_distance/1e3, relmelt(p,:), 'o-', 'color', plotcolor1, 'markerfacecolor', plotcolor1);
+plot(snap_distance/1e3, relmelt_noTemp(p,:), 'o-', 'color', plotcolor2, 'markerfacecolor', plotcolor2);
+plot(snap_distance/1e3, relmelt_noVel(p,:), 'o-', 'color', plotcolor3, 'markerfacecolor', plotcolor3);
+end
+l = legend(ax(2), {"$\mathcal{M}$", "$U_e$", "$\Delta T_e$"}, 'location', 'northwest','interpreter', 'latex', 'FontSize', 12);
+
+ax(2).YLim = [0.98, 1.15];
+ax(4).YLim = [0.98, 1.06];
+ax(2).XLabel.String = '$l_c$ (km)';
+ax(4).XLabel.String = '$l_c$ (km)';
+ax(2).YLabel.Interpreter = 'latex';
+ax(2).XLabel.Interpreter = 'latex';
+ax(4).YLabel.Interpreter = 'latex';
+ax(4).XLabel.Interpreter = 'latex';
+ax(2).YLabel.String = 'relative effect';
+ax(4).YLabel.String = 'relative effect';
+ax(2).XLabel.FontSize = 12;
+ax(2).YLabel.FontSize = 12;
+ax(4).XLabel.FontSize = 12;
+ax(4).YLabel.FontSize = 12;
+
+
+%plot labels
+ta = text(ax(1), -10, 38.4, '(a)', 'Interpreter', 'latex', 'FontSize', 12);
+tb = text(ax(2), -10, 1.166, '(b)', 'Interpreter', 'latex', 'FontSize', 12);
+tc = text(ax(3), -10, 69.6, '(c)', 'Interpreter', 'latex', 'FontSize', 12);
+td = text(ax(4), -10, 1.072, '(d)', 'Interpreter', 'latex', 'FontSize', 12);
+
+
+
+%
+% Save flag
+%
+if saveflag
+saveas(gcf, 'plots/figure12.eps', 'epsc')
 end
